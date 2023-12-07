1f6311bd15ca028a796d3f8a6d9b5a70feebfa31
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 14:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", (e) => {
+    enter(e);
+  });
+
+  square.addEventListener("mouseleave", () => {
+    leave();
+  });
+});
+
+function enter(e) {
+  let hovered = e.target;
+  console.log(hovered);
+
+  squares.forEach((square) => {
+    if (hovered.id !== square.id) {
+      square.classList.add("active");
+    } else {
+      square.classList.remove("active");
+    }
+  });
+}
+
+function leave() {
+  squares.forEach((square) => {
+    square.classList.remove("active");
+  });
+}
